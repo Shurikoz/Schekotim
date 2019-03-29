79734a30e675a46d2c654697a43f9b3d12d6cc3c
shurikoz <shurikoz@narod.ru>
2019-03-29 21:40:36 +0300

    metrika

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -30,6 +30,22 @@ AppAsset::register($this);
     <?php $this->head() ?>
 </head>
 <body>
+<?php
+$script = <<< JS
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(53045104, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+    });
+JS;
+$this->registerJs($script, yii\web\View::POS_READY);
+?>
+<noscript><div><img src="https://mc.yandex.ru/watch/53045104" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 <?php $this->beginBody() ?>
 
 <div id="wrapper">
@@ -133,20 +149,6 @@ AppAsset::register($this);
     </div>
 </div>
 
-<?php
-$script = <<< JS
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(53045104, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-    });
-JS;
-$this->registerJs($script, yii\web\View::POS_READY);
-?>
 <?php $this->endBody() ?>
 </body>
 </html>

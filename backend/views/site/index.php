e79c41f4f6d5175b22dee6387222d24ee486ade6
shurikoz <shurikoz@narod.ru>
2020-08-23 14:51:10 +0300

    новый дизайн главного меню

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -52,52 +52,101 @@ $leader = Yii::$app->user->can('leader');
 <br>
 <div class="row">
     <?php if ($admin || $administrator || $leader) { ?>
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <div class="box">
-                <?= Html::a('Создать карту', ['card/create'], ['class' => 'btn btn-lg btn-green center-block']) ?>
+                <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/compose.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Создать карту</h3>
+                        </div>
+                    </div>',
+                        ['/card/create'], ['class' => 'menuLink']) ?>
             </div>
         </div>
     <?php } ?>
 
     <?php if ($admin || $podolog || $dermatolog || $administrator || $leader) { ?>
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <div class="box">
-                <?= Html::a('Карты', ['/card'], ['class' => 'btn btn-lg btn-info center-block']) ?>
+                <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/folder.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Список карт</h3>
+                        </div>
+                    </div>',
+                        ['/card'], ['class' => 'menuLink']) ?>
             </div>
         </div>
         <?php if ($podolog || $dermatolog) { ?>
-            <div class="col-md-6 col-sm-6 col-sm-6">
+            <div class="col-md-4 col-sm-6">
                 <div class="box">
-                    <?= Html::a('Календарь', ['/event'], ['class' => 'btn btn-lg btn-primary center-block']) ?>
+                    <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/calendar.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Календарь</h3>
+                        </div>
+                    </div>',
+                        ['/event'], ['class' => 'menuLink']) ?>
                 </div>
             </div>
         <?php } ?>
     <?php } ?>
 
     <?php if ($admin || $smm || $leader) { ?>
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <div class="box">
-                <?= Html::a('Фото работ (из карт клиентов)', ['/photo'], ['class' => 'btn btn-lg btn-info center-block']) ?>
+                <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/polaroids.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Фото работ<br>(из посещений)</h3>
+                        </div>
+                    </div>',
+                    ['/photo'], ['class' => 'menuLink']) ?>
             </div>
         </div>
     <?php } ?>
 
     <?php if ($admin || $administrator || $leader) { ?>
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <div class="box">
-                <?= Html::a('Пропущенные посещения', ['visit/missed'], ['class' => 'btn btn-lg btn-danger center-block']) ?>
+                <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/check.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Запланированные посещения</h3>
+                        </div>
+                    </div>',
+                    ['/visit/planned'], ['class' => 'menuLink']) ?>
             </div>
         </div>
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <div class="box">
-                <?= Html::a('Запланированные посещения', ['visit/planned'], ['class' => 'btn btn-lg btn-warning center-block']) ?>
+                <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/x.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Пропущенные посещения</h3>
+                        </div>
+                    </div>',
+                    ['/visit/missed'], ['class' => 'menuLink']) ?>
             </div>
         </div>
     <?php } ?>
     <?php if ($admin || $administrator || $leader) { ?>
-        <div class="col-md-6  col-sm-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <div class="box">
-                <?= Html::a('Посещения без фото', ['visit/nophotos'], ['class' => 'btn btn-lg btn-default center-block']) ?>
+                <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/nophoto.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Посещения без фото</h3>
+                        </div>
+                    </div>',
+                    ['/visit/nophotos'], ['class' => 'menuLink']) ?>
             </div>
         </div>
     <?php } ?>
@@ -105,14 +154,28 @@ $leader = Yii::$app->user->can('leader');
 <?php if ($admin || $leader) { ?>
     <hr>
     <div class="row">
-        <div class="col-md-6 col-sm-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <div class="box">
-                <?= Html::a('Логи', ['/log'], ['class' => 'btn btn-lg btn-info center-block']) ?>
+                <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/cone.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Логи</h3>
+                        </div>
+                    </div>',
+                    ['/log'], ['class' => 'menuLink']) ?>
             </div>
         </div>
-        <div class="col-md-6 col-sm-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <div class="box">
-                <?= Html::a('Шаблоны для специалистов', ['/problem'], ['class' => 'btn btn-lg btn-info center-block']) ?>
+                <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/stack.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Шаблоны для специалистов</h3>
+                        </div>
+                    </div>',
+                    ['/problem'], ['class' => 'menuLink']) ?>
             </div>
         </div>
     </div>
@@ -120,15 +183,18 @@ $leader = Yii::$app->user->can('leader');
 <hr>
 <?php if ($admin) { ?>
     <div class="row">
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <div class="box">
-                <?= Html::a('Обращения', ['pages/support'], ['class' => 'btn btn-lg btn-green center-block']) ?>
-            </div>
-        </div>
-        <div class="col-md-6 col-sm-6">
-            <div class="box">
-                <p>Новых обращений: <b><?= $viewed ?></b></p>
-                <p>Нерешенных обращений: <b><?= $result ?></b></p>
+                <?= Html::a('
+                    <div class="btnMenu">
+                        <div class="menuIcon text-center">' . Html::img('/images/icons/circlecompass.png') . '</div>
+                        <div class="menuText text-center">
+                            <h3>Поддержка</h3>
+                        </div>
+                        <div>Новых обращений: <b>' . $viewed  . '</b></div>
+                        <div>Нерешенных обращений: <b>' . $result . '</b></div>
+                    </div>',
+                    ['/pages/support'], ['class' => 'menuLink']) ?>
             </div>
         </div>
     </div>
